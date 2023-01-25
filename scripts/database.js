abe9e00d6f385d09baa55c3dfe737d90bbbaae74
Ernie Fabian <erniefabian3rd@gmail.com>
2023-01-25 13:07:32 -0600

    First version of bookings code

---
+++ scripts/database.js
@@ -0,0 +1,141 @@
+/*
+    Create a database array that holds objects with properties:
+    -Venues
+        > id - int
+        > Name of venue - string
+        > Address - string
+        > Square footage - int
+        > Max capacity - int
+    -Bands
+        > id - int
+        > Band name - string
+        > # of members - int
+        > Genre - string
+        > Year formed as band - int
+    -Bookings
+        > id - int
+        > Band id - int
+        > Venue id - int
+        > Date of booking - string
+
+    Export a copy of each array
+*/
+
+const database = {
+    venues: [{
+        id: 1,
+        name: "Exit/In",
+        address: "2208 Ellliston Place",
+        squareFoot: 4110,
+        capacity: 500
+
+    }, {
+        id: 2,
+        name: "The End",
+        address: "2219 Elliston Place",
+        squareFoot: 2376,
+        capacity: 200
+
+    }, {
+        id: 3,
+        name: "Marathon Music Works",
+        address: "1402 Clinton Street",
+        squareFoot: 14000,
+        capacity: 1800
+        
+    }, {
+        id: 4,
+        name: "Basement East",
+        address: "917 Woodland Street",
+        squareFoot: 5000,
+        capacity: 400
+        
+    }, {
+        id: 5,
+        name: "Mercy Lounge",
+        address: "1 Cannery Row",
+        squareFoot: 4500,
+        capacity: 500
+        
+    }],
+
+    bands: [{
+        id: 1,
+        name: "Manchester Orchestra",
+        memberCount: 5,
+        genre: "Indie Rock",
+        yearFormed: 2004
+
+    }, {
+        id: 2,
+        name: "Norma Jean",
+        memberCount: 5,
+        genre: "Metalcore",
+        yearFormed: 1997
+    }, {
+        id: 3,
+        name: "Thrice",
+        memberCount: 4,
+        genre: "Rock",
+        yearFormed: 1998
+
+    }, {
+        id: 4,
+        name: "Noah Gundersen",
+        memberCount: 1,
+        genre: "Indie Folk",
+        yearFormed: 2005
+
+    }, {
+        id: 5,
+        name: "Advent",
+        memberCount: 4,
+        genre: "Hardcore",
+        yearFormed: 2005
+
+    }],
+
+    bookings: [{
+        id: 1,
+        bandId: 1,
+        venueId: 3,
+        date: "February 3rd, 2023"
+
+    }, {
+        id: 2,
+        bandId: 2,
+        venueId: 1,
+        date: "February 10th, 2023"
+
+    },{
+        id: 3,
+        bandId: 3,
+        venueId: 5,
+        date: "February 17th, 2023"
+        
+    },{
+        id: 4,
+        bandId: 4,
+        venueId: 4,
+        date: "February 24th, 2023"
+        
+    },{
+        id: 5,
+        bandId: 5,
+        venueId: 2,
+        date: "March 3rd, 2023"
+        
+    }]
+}
+
+export const getVenues = () => {
+    return database.venues.map(venue => ({ ...venue }))
+}
+
+export const getBands = () => {
+    return database.bands.map(band => ({ ...band }))
+}
+
+export const getBookings = () => {
+    return database.bookings.map(booking => ({ ...booking }))
+}
---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+/*
+GOAL:
+    Create a website that lists:
+    - Venues (accomodates 2 bands per night)
+        > Name of venue
+        > Address
+        > Square footage
+        > Max capacity
+        > Format: List at bottom side by side with bands list
+    - Bands (can play 1-2 sets per night)
+        > Band name
+        > # of members
+        > Genre
+        > Year formed as band
+        > Format: List at bottom side by side with venues list
+    - Bookings
+        > Band name
+        > Venue name
+        > Date of booking
+        > Format: List at top = "BAND is playing at VENUE on DATE"
+
+ALGORTITHM:
+    - Create an ERD that shows the relationship of venues/bands/bookings
+    - Create necessary modules
+        > database.js to store each array that must be created
+        > Venues.js to iterate the venues array and put in HTML <ul>
+        > Bands.js to iterate the bands array and put in HTML <ul>
+        > Bookings.js to iterate the bookings array and put in HTML <ul>
+            using string interpolation to meet required format above
+        > main.js to compile all of the individual lists into HTML
+            and target index.html container class/id using .querySelector and .innerHTML
+        > index.html to create a basic HTML structure and set up a "catch point" container
+            for previous step
+        > main.css to style using flexbox to the necessary format requirement above
+    - Make each module exportable by using .map function to create
+        copies of each array and import to respective modules
+    - Create Event Listeners for each module to display window alerts:
+        > Venues: displays all bands that have booked venue
+        > Bands: displays all venues the band is playing
+        > Bookings: displays band name, genre, year formed, and # of members
+    - Style website to meet criteria and STRETCH: make it cool
+*/
